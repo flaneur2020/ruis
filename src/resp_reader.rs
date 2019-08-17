3f385c93c704f5c669ebd9e34539eaa8ea7480af
Li Yazhou <me.ssword@gmail.com>
2019-08-17 14:44:42 +0800

    tune the reader

--- src/resp_reader.rs
+++ src/resp_reader.rs
@@ -2,6 +2,8 @@ use std::io;
 use std::str::FromStr;
 use std::io::{BufReader, BufRead};
 
+// https://redis.io/topics/protocol
+
 struct RespReader<R> {
     reader: BufReader<R>
 }
@@ -38,24 +40,21 @@ impl<R: BufRead> RespReader<R> {
         let mut line = vec![];
         self.reader.read_until('\n' as u8, &mut line)?;
         if line.len() == 0 {
-            return Err(RespError::Unexpected("line empty"));
+            return Err(RespError::ParseFailed(format!("line empty")));
         }
 
         match line[0] as char {
+            ':' => {
+                let n = self.read_int(&line[1..])?;
+                return Ok(RespValue::Int(n));
+            },
             '+' => {
-                let s = std::str::from_utf8(&line[1..]).or(
-                    Err(RespError::ParseFailed(format!("bad utf8")))
-                )?;
-                return Ok(RespValue::String(String::from(s)));
+                let s = self.read_string(&line[1..])?;
+                return Ok(RespValue::String(s));
             }
             '-' => {
-                let s = std::str::from_utf8(&line[1..]).or(
-                    Err(RespError::ParseFailed(format!("bad utf8")))
-                )?;
-                return Ok(RespValue::Error(String::from(s)));
-            }
-            ':' => {
-                return self.read_int(&line[1..])
+                let s = self.read_string(&line[1..])?;
+                return Ok(RespValue::Error(s));
             }
             ch @ _ => {
                 Err(RespError::ParseFailed(format!("unexpected token: {}", ch)))
@@ -63,7 +62,18 @@ impl<R: BufRead> RespReader<R> {
         }
     }
 
-    fn read_int(&mut self, buf: &[u8]) -> Result<RespValue, RespError> {
+    fn read_string(&mut self, buf: &[u8]) -> Result<String, RespError> {
+        let s = std::str::from_utf8(buf).or(
+            Err(RespError::ParseFailed(format!("bad utf8")))
+        )?;
+        return Ok(String::from(s));
+    }
+
+    fn read_bulk_string(&mut self, buf: &[u8]) -> Result<String, RespError> {
+        return Ok(String::from("miao"))
+    }
+
+    fn read_int(&mut self, buf: &[u8]) -> Result<i64, RespError> {
         if buf.len() == 0 {
             return Err(RespError::ParseFailed(format!("malformed integer")));
         }
@@ -74,6 +84,6 @@ impl<R: BufRead> RespReader<R> {
         let n = i64::from_str(s).or(
             Err(RespError::ParseFailed(format!("parse int failed")))
         )?;
-        return Ok(RespValue::Int(n));
+        return Ok(n);
     }
 }
